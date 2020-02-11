1c67083551f3c272f83e3dc4760e85c60a03ff7c
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-02-11 04:31:44 +0000

    Check if enough time has passed before posting again

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -7,6 +7,20 @@ module.exports =
 
     getLatest: function(twitter, timer, callback)
     {
-        twitter.get()
+        twitter.get('statuses/user_timeline', {screen_name: "evil_aesthetics", count: '1', include_rts: 'false'}, function(error, tweet)
+        {
+            //console.log(tweet);
+            var createdAt = new Date(tweet[0]["created_at"]);
+            console.log(timer);
+            console.log(createdAt.getTime());
+            if(Date.now() - timer > createdAt.getTime())
+            {
+                console.log("It is");
+            }
+            else
+            {
+                console.log("It isn't");
+            }
+        })
     }
 }
