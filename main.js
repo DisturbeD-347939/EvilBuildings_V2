2decff428b3be8677c9ffe911f1534987d6105c2
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-02-11 03:33:52 +0000

    Added reddit and twitter APIs

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Require scripts
 var auth = require('./scripts/authenticate.js');
+var twitter = require('./scripts/twitter.js');
 
 //File management
 var fs = require('fs');
@@ -7,14 +8,28 @@ var fs = require('fs');
 //Get config file variables
 var keyPath = 'keys.json';
 var configData = JSON.parse(fs.readFileSync('config.json', 'utf-8'));
+var twitterDailyRate, redditDailyRate;
+
+//APIs
+var t, r;
 
 setImmediate(function()
 {
-    
+    //Transform daily rate into seconds
+    twitterDailyRate = (24/configData["twitter"][0]["daily_rate"]) * 60 * 60;
 
-    auth.run(keyPath, function()
+    //Authenticate APIs
+    auth.run(keyPath, function(data)
     {
+        r = data[0];
+        t = data[1];
         console.log("Authenticated APIs");
+
+        //Check if it's been long enough to post new tweet!
+        twitter.getLatest(t, twitterDailyRate, function(data)
+        {
+            console.log(data);
+        })
     });
 });
 
@@ -25,5 +40,5 @@ function getPost()
 
 function createPost()
 {
-    
+
 }
