4f94236d972e52bfc60180d79f408414facdd1d9
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-02-12 02:15:17 +0000

    Create reddit.js

---
+++ scripts/reddit.js
@@ -0,0 +1,8 @@
+module.exports =
+{
+    //Download and organize reddit posts from r/evilbuildings
+    collect: function(reddit, subreddit, posts_per_day, postNumber, countriesList, citiesList, callback)
+    {
+        
+    }
+}
